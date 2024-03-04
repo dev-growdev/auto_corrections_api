e3039c7ef297b4317b36fd3ec3a2171362080f5c
Growdev Desenvolvimento <dev@growdev.com.br>
2024-03-04 20:13:34 -0300

    add logs

--- src/jobs.ts
+++ src/jobs.ts
@@ -3,6 +3,6 @@ import { SQLJob } from './jobs/sql.job';
 
 export class Jobs {
     init() {
-        scheduleJob('* * * * *', () => new SQLJob().execute())
+        scheduleJob('0 * * * *', () => new SQLJob().execute())
     }
 }
